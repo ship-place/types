232c834c0932bc58d93f1a2692abc1a2d662541f
Lexx Xell <lexxxell007@gmail.com>
2023-03-10 20:21:21 +0400

    Updated

--- common/clients/changes-message-patterns.enum.ts
+++ common/clients/changes-message-patterns.enum.ts
@@ -1,6 +1,6 @@
 import { Pattern } from '@ship-place/ms-client-module/src/@types/pattern.type';
 
-export const ChangesMesagePattern: Pattern = {
+export const MsChangesMessagePatterns: Pattern = {
   ping: 'changes.ping',
   get: 'changes.get',
   create: 'changes.create',
